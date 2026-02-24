98046437b4639ca42b021544373e71c529ba6f26
bailin wei <weibailin@hotmail.com>
2026-02-24 09:58:24 +0800

    add fall in from right to left animations for app-tile and app-author

--- src/app/components/author/author.component.ts
+++ src/app/components/author/author.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
 
@@ -7,13 +7,33 @@ import { DataService } from 'src/app/services/data.service';
   templateUrl: './author.component.html',
   styleUrls: ['./author.component.scss'],
 })
-export class AuthorComponent {
+export class AuthorComponent implements AfterViewInit, OnDestroy {
 
   @Input() name?: string;
   @Input() source?: any;
 
+  private observer?: IntersectionObserver;
+
   constructor(
     public data: DataService,
     private router: Router,
+    private el: ElementRef
   ){}
+
+  ngAfterViewInit() {
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            this.el.nativeElement.classList.add('in-view');
+            this.observer?.disconnect();
+        }
+      });
+    }, { threshold: 0.1 }); 
+
+    this.observer?.observe(this.el.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.observer?.disconnect();
+  }
 }
--- src/app/components/tile/tile.component.ts
+++ src/app/components/tile/tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { DataService } from 'src/app/services/data.service';
@@ -9,17 +9,37 @@ import { UiService } from 'src/app/services/ui.service';
   templateUrl: './tile.component.html',
   styleUrls: ['./tile.component.scss'],
 })
-export class TileComponent {
+export class TileComponent implements AfterViewInit, OnDestroy {
 
   @Input() name?: string;
   @Input() arrow?: boolean;
   @Input() source?: any;
 
+  private observer?: IntersectionObserver;
+
   constructor(
     public ui: UiService,
     public data: DataService,
     private router: Router,
+    private el: ElementRef
   ){}
+
+  ngAfterViewInit() {
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          this.el.nativeElement.classList.add('in-view');
+          this.observer?.disconnect();
+        }
+      });
+    }, { threshold: 0.1 });
+
+    this.observer.observe(this.el.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.observer?.disconnect();
+  }
 
   search(key:any){
     this.router.navigate(['/tabs/tab3'], {
