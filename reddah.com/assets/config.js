5de7a5e81232a7601e81fad280e4679788b42417
bailin wei <weibailin@hotmail.com>
2026-03-14 20:37:34 +0800

    add reddah.com

---
+++ reddah.com/assets/config.js
@@ -0,0 +1,185 @@
+﻿// eslint-disable-next-line no-unused-vars
+var config = {
+  isRoot: true,
+  title: '名诗佳句',
+  logo: null,
+  logoLight: null,
+  login:'https://portal.xlcc.club/passport/login',
+  help:'https://wd889.xlcc.live/common/',
+  channelCode: '000165',
+  android: {
+    url: 'https://download.fnxz.live/000165',
+    appendCode: false
+  },
+  ios: {
+    url: 'https://portal.xlcc.club/passport/login?channel_code=000165',
+    appendCode: true
+  },
+  windows: {
+    url: 'https://portal.xlcc.club/passport/login?channel_code=000165',
+    appendCode: true
+  },
+  windows10: {
+    url: 'https://download.fnxz.live/download/fnjs_2.3.5_x64_zh-CN.msi',
+    appendCode: false
+  },
+  macos: {
+    url: 'https://download.fnxz.live/download/fnjs_2.3.5_universal.dmg',
+    appendCode: false
+  },
+  salesiq: 'siq07c0ab2a503f5e2d405f254bf32a349882684934e19915ceb47f2872f4c4ff1e',
+  privacy: 'privacy.html',
+  terms: 'terms.html',
+  telegram: 'https://t.me/fnvpngf',
+  gtm: {
+    id: 'GTM-W7W58687'
+  },
+  plans: [
+    {
+      name: '周卡',
+      price: 12,
+      descs: [
+        {
+          text: '· 新人专属 低价尝鲜',
+          import: true
+        },
+        {
+          text: '· 高速传输 不限流量'
+        },
+        {
+          text: '· 优质线路 全球节点'
+        },
+        {
+          text: '· 实力团队 稳定运营'
+        },
+        {
+          text: '· 全程加密 多端登录'
+        }
+      ]
+    },
+    {
+      name: '月卡',
+      price: 30,
+      descs: [
+        {
+          text: '· 经典卡种 随享随用',
+          import: true
+        },
+        {
+          text: '· 高速传输 不限流量'
+        },
+        {
+          text: '· 优质线路 全球节点'
+        },
+        {
+          text: '· 实力团队 稳定运营'
+        },
+        {
+          text: '· 全程加密 多端登录'
+        }
+      ]
+    },
+    {
+      name: '季卡',
+      price: 81,
+      original_price: 100,
+      label: '立省¥19',
+      descs: [
+        {
+          text: '· 随机赠送    5-15天',
+          import: true
+        },
+        {
+          text: '· 高速传输 不限流量'
+        },
+        {
+          text: '· 优质线路 全球节点'
+        },
+        {
+          text: '· 实力团队 稳定运营'
+        },
+        {
+          text: '· 全程加密 多端登录'
+        }
+      ]
+    },
+    {
+      name: '半年卡',
+      price: 145,
+      original_price: 210,
+      label: '立省¥65',
+      descs: [
+        {
+          text: '· 随机赠送  20-40天',
+          import: true
+        },
+        {
+          text: '· 高速传输 不限流量'
+        },
+        {
+          text: '· 优质线路 全球节点'
+        },
+        {
+          text: '· 实力团队 稳定运营'
+        },
+        {
+          text: '· 全程加密 多端登录'
+        }
+      ]
+    },
+    {
+      name: '年卡',
+      price: 252,
+      original_price: 430,
+      label: '立省¥178',
+      descs: [
+        {
+          text: '· 随机赠送  50-90天',
+          import: true
+        },
+        {
+          text: '· 高速传输 不限流量'
+        },
+        {
+          text: '· 优质线路 全球节点'
+        },
+        {
+          text: '· 实力团队 稳定运营'
+        },
+        {
+          text: '· 全程加密 多端登录'
+        }
+      ]
+    },
+    {
+      name: '两年卡',
+      price: 432,
+      original_price: 920,
+      label: '立省¥488',
+      descs: [
+        {
+          text: '· 随机赠  160-240天',
+          import: true
+        },
+        {
+          text: '· 高速传输 不限流量'
+        },
+        {
+          text: '· 优质线路 全球节点'
+        },
+        {
+          text: '· 实力团队 稳定运营'
+        },
+        {
+          text: '· 全程加密 多端登录'
+        }
+      ]
+    }
+  ],
+  LA: {
+    id: '',
+    ck: ''
+  }
+}
+
+
