b58d8a5ffa3982eddc859415e85484325479d14d
bailin wei <weibailin@hotmail.com>
2026-05-30 17:13:33 +0800

    fix data not found issue

--- src/app/pages/by-id-custom-list/list.page.ts
+++ src/app/pages/by-id-custom-list/list.page.ts
@@ -159,7 +159,7 @@ export class ListPage {
   CheckIsPlayList(){
     this.localList.forEach((poem:any) => {
       let fullData = this.data.JsonData.filter((j:any)=>j.id===poem.id)[0];
-      if(fullData.audio!=null){
+      if(fullData&&fullData.audio!=null){
         poem.audio = fullData.audio;
         this.noAudio = false;
       }
